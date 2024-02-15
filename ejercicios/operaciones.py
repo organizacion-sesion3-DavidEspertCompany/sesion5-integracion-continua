127d47d50c5fe5c7acccceb6320ab52dc80bade4
David Espert C <david.espert@gmail.com>
2024-02-15 17:32:40 +0100

    Update operaciones.py

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -3,3 +3,8 @@ def suma(x,y):
 
     # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente
     return x + y
+
+
+def es_par(numero):
+    # Comprueba si un número dado es par.
+    return numero % 2 == 0
